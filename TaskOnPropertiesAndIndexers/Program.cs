17e99b8b9ce2536295f925ffd42ab710422057ce
Султанбаев Ерсын Балабулатович <@corp.itstep>
2019-01-24 21:01:32 +0600

    Добавьте файлы проекта.

---
+++ TaskOnPropertiesAndIndexers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOnPropertiesAndIndexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loverRange=0;
+            int upRange=0;
+            bool resultOfFirstConversion, resultOfSecondConversion;
+            bool check=false;
+            while (check==false)
+            {
+                Console.Write("Введите начало диапазона: ");
+                resultOfFirstConversion = int.TryParse(Console.ReadLine(), out loverRange);
+
+                Console.Write("Введите конец диапазона: ");
+                resultOfSecondConversion = int.TryParse(Console.ReadLine(), out upRange);
+                if (resultOfFirstConversion == true && resultOfSecondConversion == true)
+                {
+                    check = true;
+                }
+                else
+                {
+                    Console.WriteLine("Введите цифры!!!");
+                }
+            }
+                RangeOfArray array = new RangeOfArray(loverRange, upRange);
+                Random random = new Random();
+
+            /*Метод заполнеиния массива*/
+
+            if (loverRange > upRange)
+            {
+                Swap(ref loverRange, ref upRange);
+            }
+
+
+            for (int i=loverRange; i<upRange; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+
+            /*Отображения массива*/
+
+            for (int i = loverRange; i < upRange; i++)
+            {
+                Console.Write(array[i]+ " ");
+            }
+            Console.WriteLine();
+
+            array.ShowClassArray();
+
+            Console.ReadLine();
+        }
+        static void Swap(ref int value1, ref int value2)
+        {
+            int tmp = value1;
+            value1 = value2;
+            value2 = tmp;
+        }
+    }
+}
---
+++ TaskOnPropertiesAndIndexers/RangeOfArray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOnPropertiesAndIndexers
+{
+    class RangeOfArray
+    {
+        private int[] array;
+
+        public RangeOfArray(int loverRange, int upRange)
+        {
+            if (loverRange > upRange)
+            {
+                Swap(ref loverRange, ref upRange);
+            }
+
+            LoverRange = loverRange;
+            UpRange = upRange;
+            array = new int[upRange - loverRange];
+        }
+
+        /*Метод метод обмена значениями полей*/
+        public void Swap(ref int value1, ref int value2)
+        {
+            int tmp = value1;
+            value1 = value2;
+            value2 = tmp;
+        }
+
+        public int LoverRange { get; set; }
+        public int UpRange { get; set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (LoverRange > 0)
+                    return array[Math.Abs(index) - LoverRange];
+                else
+                    return array[Math.Abs(LoverRange) + index];
+            }
+            set
+            {
+                if (LoverRange > 0)
+                    array[Math.Abs(index) - LoverRange] = value;
+                else
+                    array[Math.Abs(LoverRange) + index] = value;
+            }
+        }
+
+        public void ShowClassArray()
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+      
+    }
+}
